e5b4caa6cec828b62d523cfdfe3d81820951fb22
HenkkaL <henriklyra@gmail.com>
2017-01-18 16:28:40 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -33,7 +33,6 @@ chmod -R a+rX htdocs
 cd htdocs/$PROJECT_FOLDER
 php -r "copy 'https://getcomposer.org/installer', 'composer-setup.php' ;"
 php composer-setup.php --version=1.3.0
-php -r "unlink('composer-setup.php');"
 exit"
  
 echo "Valmis! Sovelluksesi on nyt valmiina osoitteessa $USERNAME.users.cs.helsinki.fi/$PROJECT_FOLDER"
