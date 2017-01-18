4720099377a143f194408a1a13d911a9c58696d2
HenkkaL <henriklyra@gmail.com>
2017-01-18 16:15:15 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,7 +31,7 @@ echo "Asetetaan käyttöoikeudet ja asennetaan Composer..."
 ssh $USERNAME@users.cs.helsinki.fi "
 chmod -R a+rX htdocs
 cd htdocs/$PROJECT_FOLDER
-curl -sS https://getcomposer.org/download/1.3.0/composer | php
+curl -sS https://getcomposer.org/download/1.3.0/composer.phar | php
 php composer.phar install
 exit"
 
