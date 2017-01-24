acf356ce68aba82a3d4061a21ca6f9ced50c1a3a
HenkkaL <henriklyra@gmail.com>
2017-01-24 21:49:35 +0200

    Alustavat sivupohjat pääosin luot

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,43 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+   	  View::make('suunnitelmat/etusivu.html');
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      View::make('helloworld.html');
+    }
+
+    public static function etusivu(){
+      View::make('suunnitelmat/etusivu.html');
+    }
+
+    public static function lenkki_lista(){
+      View::make('suunnitelmat/lenkki_lista.html');
+    }
+
+    public static function lenkki_esittely(){
+      View::make('suunnitelmat/lenkki_esittely.html');
+    }
+
+    public static function lenkki_muokkaus(){
+      View::make('suunnitelmat/lenkki_muokkaus.html');
+    }
+
+    public static function omasivu(){
+      View::make('suunnitelmat/omasivu.html');
+    }
+
+    public static function register(){
+      View::make('suunnitelmat/register.html');
+    }
+
+    public static function login(){
+      View::make('suunnitelmat/login.html');
+    }
+
+    public static function lenkki_kanta(){
+      View::make('suunnitelmat/lenkki_kanta.html');
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,35 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+
+    $routes->get('/etusivu', function() {
+    HelloWorldController::etusivu();
+  });
+
+    $routes->get('/lenkki_lista', function() {
+    HelloWorldController::lenkki_lista();
+  });
+
+    $routes->get('/lenkki_esittely', function() {
+    HelloWorldController::lenkki_esittely();
+  });
+
+    $routes->get('/lenkki_muokkaus', function() {
+    HelloWorldController::lenkki_muokkaus();
+  });
+
+    $routes->get('/omasivu', function() {
+    HelloWorldController::omasivu();
+  });
+
+    $routes->get('/register', function() {
+    HelloWorldController::register();
+  });
+
+      $routes->get('/login', function() {
+    HelloWorldController::login();
+  });        
+
+      $routes->get('/lenkki_kanta', function() {
+    HelloWorldController::lenkki_kanta();
+  });        
